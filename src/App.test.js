dcfd667a95dc1126d693e8c543b7895b3ca508ae
Sean Dolan <sdolan@theclimateservice.com>
2020-01-12 10:55:26 -0500

    everything through video 84, with hot reload setup.

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,7 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Works from Docker compose!/i);
   expect(linkElement).toBeInTheDocument();
 });
+
