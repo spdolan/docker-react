c14e45076f787e23a11d9f96ec46dc3cb7496bc9
Sean Dolan <sdolan@theclimateservice.com>
2020-01-27 20:44:26 -0500

    updated really quickly.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Keep closed <code>src/App.js</code> and save to reload.
+          Another update in <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
